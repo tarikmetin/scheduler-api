72c6502514c3340f376e23a2c2d958b1f59046df
tarikmetin <tarik.metin1@gmail.com>
2024-04-01 15:03:41 -0300

    variable-change

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 const createToken = (_id) => {
   // Creates a token and returns it
-  return jwt.sign({ _id }, process.env.Secret, { expiresIn: "3d" });
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
 };
 
 //Login user
